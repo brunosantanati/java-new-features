04c4add2e0dab676f0476d7c3144b098f1cd36d7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-05-26 12:42:47 -0300

    Trying to use JMH (not working properly)

--- src/main/java/me/bruno/santana/incubator/VectorApiDemo.java
+++ src/main/java/me/bruno/santana/incubator/VectorApiDemo.java
@@ -1,23 +1,43 @@
 package me.bruno.santana.incubator;
 
 import jdk.incubator.vector.IntVector;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 //Add VM options: --add-modules jdk.incubator.vector
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+//@Warmup(iterations = 3)
+//@Measurement(iterations = 8)
 public class VectorApiDemo {
 
-    public static void main(String[] args) {
-        sumTwoArrays();
+    public static void main(String[] args) throws RunnerException {
+        /*sumTwoArrays();
         System.out.println("--------------------------------------------------");
-        sumTwoArraysUsingVectorApi();
+        sumTwoArraysUsingVectorApi();*/
+        Options opt = new OptionsBuilder()
+                .include(VectorApiDemo.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
     }
 
-    private static void sumTwoArrays() {
-        System.out.println("sumTwoArrays");
-        Instant start = Instant.now();
+    @Benchmark
+    public static void sumTwoArrays() {
+        /*System.out.println("sumTwoArrays");
+        Instant start = Instant.now();*/
 
         int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         int[] array2 = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
@@ -28,16 +48,17 @@ public class VectorApiDemo {
             result[i] = array1[i] + array2[i];
         }
 
-        Instant end = Instant.now();
+        /*Instant end = Instant.now();
         System.out.println(Duration.between(start, end).toMillis() + " ms");
-        System.out.println(Duration.between(start, end).toNanos() + " ns");
+        System.out.println(Duration.between(start, end).toNanos() + " ns");*/
 
         System.out.println(Arrays.toString(result));
     }
 
-    private static void sumTwoArraysUsingVectorApi(){
-        System.out.println("sumTwoArraysUsingVectorApi");
-        Instant start = Instant.now();
+    @Benchmark
+    public static void sumTwoArraysUsingVectorApi(){
+        /*System.out.println("sumTwoArraysUsingVectorApi");
+        Instant start = Instant.now();*/
 
         int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         int[] array2 = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
@@ -49,9 +70,9 @@ public class VectorApiDemo {
         var vectorResult = vector1.add(vector2);
         vectorResult.intoArray(result, 0);
 
-        Instant end = Instant.now();
+        /*Instant end = Instant.now();
         System.out.println(Duration.between(start, end).toMillis() + " ms");
-        System.out.println(Duration.between(start, end).toNanos() + " ns");
+        System.out.println(Duration.between(start, end).toNanos() + " ns");*/
 
         System.out.println(Arrays.toString(result));
     }
